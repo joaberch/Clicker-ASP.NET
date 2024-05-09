8cf17a6cc836c0ef0a9f4b3cc1e747422eaf7e1f
cycoborchp <joachimsimeongabriel.berchel@eduvaud.ch>
2024-05-09 17:18:04 +0200

    Add a functional copy

--- Projet/FastClickedApp-main/FastClieckWebAppMVC/Models/Player.cs
+++ Projet/FastClickedApp-main/FastClieckWebAppMVC/Models/Player.cs
@@ -5,11 +5,11 @@
         // ID du joueur
         public int Id { get; set; }
 
-		// Pseudo du joueur 
-		public string username { get; set; }
+        // Pseudo du joueur 
+        public string username { get; set; }
 
-		// nbr of click
-		public int clickNbr { get; set; }
+        // nbr of click
+        public int clickNbr { get; set; }
 
         // nbr of restart
         public int restartNbr { get; set; }
